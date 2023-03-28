5b033694055a5934f80a591c1d93a1b409eee047
AGlanborgBTH <agebnr1@gmail.com>
2023-03-28 16:55:45 +0200

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,6 +1,8 @@
 if not exist "C:\licenses\" mkdir "C:\licenses"
 if not exist "C:\licenses\licenses.sqlite" xcopy ".\db\licenses.sqlite" "C:\licenses\"
 
+docker-compose pull
+
 start chrome "http://localhost:8080/"
 
 docker-compose up
